90d986150f97d8c02f11413374aa5191dce51598
Taoyu Li <taoyl@microsoft.com>
2018-11-09 01:14:51 +0000

    Code fix

--- common/producerstatetable.cpp
+++ common/producerstatetable.cpp
@@ -248,16 +248,46 @@ void ProducerStateTable::apply_temp_view()
         if (needSet) keysToSet.emplace_back(key);
         else m_tempViewState.erase(key);                               // If exactly match, no need to sync new state to StateHash in DB
     }
+    // Objects that do not exist currently need to be created
+    for (auto const & kfvPair : m_tempViewState)
+    {
+        const string& key = kfvPair.first;
+        if (currentState.find(key) == currentState.end())
+        {
+            keysToSet.emplace_back(key);
+        }
+    }
 
     // Assembly redis command args into a string vector
+    //
+    // Sample args format:
+    // KEYS:
+    //   SAMPLE_CHANNEL
+    //   SAMPLE_KEY_SET
+    //   SAMPLE_DEL_KEY_SET
+    //   _SAMPLE:key_0
+    //   _SAMPLE:key_1
+    // ARGV:
+    //   G   (String to be published to channel)
+    //   2   (Count of objects to set)
+    //   key_0
+    //   key_1
+    //   0   (Count of objects to del)
+    //   2   (Count of A/V pair of object 0)
+    //   attribute_0
+    //   value_0
+    //   attribute_1
+    //   value_1
+    //   1   (Count of A/V pair of object 1)
+    //   attribute_0
+    //   value_0
     vector<string> args;
     args.emplace_back("EVALSHA");
-    args.emplace_back(m_shaSet);
+    args.emplace_back(m_shaApplyView);
     args.emplace_back(to_string(m_tempViewState.size() + 3));
     args.emplace_back(getChannelName());
     args.emplace_back(getKeySetName());
     args.emplace_back(getDelKeySetName());
-    args.emplace_back(getStateHashPrefix() + getTableName());
 
     vector<string> argvs;
     argvs.emplace_back("G");
@@ -294,6 +324,7 @@ void ProducerStateTable::apply_temp_view()
         m_pipe->flush();
     }
 
+    // Clear state, temp view operation is now finished
     m_tempViewState.clear();
     m_tempViewActive = false;
 }
